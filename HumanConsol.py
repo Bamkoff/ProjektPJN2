71939d29e4cea5ecf0b934abff445ec5f6ddb675
Bankoff <bartek.fabisiak1@gmail.com>
2020-01-23 19:28:52 +0100

    Dokończenie podstawowych funkcjonalności. Wymaga testow. Dalszy rozwuj to rozpoznawanie mowy.

--- HumanConsol.py
+++ HumanConsol.py
@@ -1,8 +1,10 @@
 import ply.lex as lex
 import ply.yacc as yacc
 import subprocess
-import shlex
 import webbrowser
+import re
+import os
+from sys import platform
 
 tokens = (
     'OPERATE',
@@ -10,9 +12,10 @@ tokens = (
     'OBJECT'
 )
 
+
 def t_OPERATE(t):
-    r'^(O|o)pen|(C|c)lose '
-    if t.value[1] == 'p':
+    r'((P|p)leas|(C|c)an(y|Y)ou)?((O|o)pen|(C|c)lose|(S|s)top|(K|k)ill|(S|s)tart)'
+    if re.search(r'((o|O)pen|(S|s)tart)', t.value) is not None:
         t.value = 0
     else:
         t.value = 3
@@ -20,23 +23,27 @@ def t_OPERATE(t):
 
 
 def t_TYPE(t):
-    r'(P|p)age|(a|A)pp|(d|D)ocument'
-    if t.value[1] == 'a':
+    r'(P|p)age|(W|w)ebpage|(S|s)ite|(a|A)pp(lication)?|(d|D)oc(ument)?|(F|f)ile|(P|p)rogram'
+    if t.value[1] == 'a' or t.value[1] == 'e' or (t.value[1] == 'i' and t.value[2] == 't'):
         t.value = 1
-    elif t.value[1] == 'p':
+    elif t.value[1] == 'p' or t.value[1] == 'r':
         t.value = 0
     else:
         t.value = 2
     return t
 
-t_OBJECT = r'[A-Za-z0-9\.]+$'
+
+def t_error(t):
+    t.type = t.value[0]
+    t.value = t.value[0]
+    t.lexer.skip(1)
+    return t
+
+
+t_OBJECT = r'[A-Za-z0-9\./_\-\:\\]+$'
 t_ignore = ' \t'
 data = 'Open app ls'
 lexer = lex.lex()
-lexer.input(data)
-#
-# for tok in lexer:
-#     print(tok)
 
 
 def p_error(p):
@@ -47,29 +54,46 @@ def p_command(p):
     'command : OPERATE TYPE OBJECT'
     action = p[1] + p[2]
     # Close page/document
-    if action > 3:
-        print('Cannot do that.')
+    try:
+        if action > 3:
+            print('Cannot do that.')
     # Open app
-    elif action == 0:
-        arg = shlex.split(p[3])
-        p = subprocess.Popen(arg)
-        print(p)
+        elif action == 0:
+            if platform[0] == 'd':
+                subprocess.call(('open', p[3]))
+            elif platform[0] == 'w':
+                os.startfile(p[3])
+            else:
+                subprocess.call(('xdg-open', p[3]))
     # open page
-    elif action == 1:
-        print('Open page ' + p[3])
-        webbrowser.open("https://www." + p[3], new=0)
+        elif action == 1:
+            print('Open page ' + p[3])
+            webbrowser.open("https://www." + p[3], new=0)
     # open document
-    elif action == 2:
+        elif action == 2:
         #to do
-        print('Open document' + p[3])
+            if platform[0] == 'd':
+                subprocess.call(('open', p[3]))
+            elif platform[0] == 'w':
+                os.startfile(p[3])
+            else:
+                subprocess.call(('xdg-open', p[3]))
     # close app
-    else:
-        print('Close app' + p[3])
+        else:
+            print('Close app' + p[3])
+            if platform[0] == "w":
+                subprocess.Popen("TASKKILL /F /IM " + p[3])
+            elif platform[0] == "l":
+                output = subprocess.Popen("ps -ef | grep " + p[3])
+                pid = re.search(r'\d', output)
+                subprocess.Popen("kill -9 " + pid)
+    except Exception as e:
+        print(e)
 
 parser = yacc.yacc()
 
 while True:
     s = input('What can i do for you?\n')
-    if s[0] == 'Bye':
+    if re.search(r'^((B|b)ye|(G|g)oodbey|(e|E)xit)$', s) is not None:
         break
     parser.parse(s)
